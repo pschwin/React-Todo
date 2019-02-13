583c9fad42e80b709377833b1167c44dca504bdf
Patrick <patrick.schwindt14@gmail.com>
2019-02-13 15:08:26 -0700

    What I have so far

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,6 +1,8 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
 import React from 'react';
+import Todo from './components/TodoComponents/Todo';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 const todoList = [
     {
@@ -27,5 +29,41 @@ const todoList = [
         completed: false 
      }
 ];
+
+class TodoList extends React.Component{
+   constructor(){
+      super ();
+      this.state = {
+         todos: todos,
+         task: '',
+         id: '',
+         completed: ''
+      };
+   }
+
+   addToDo = event =>{
+      event.preventDefault();
+      console.log(event.target);
+
+      const newToDo ={
+         task: this.state.task,
+         id: this.state.id,
+         completed: this.state.completed
+      };
+
+
+   }
+
+   render() {
+      return(
+         <div>
+            <h1>To-Do List MVP</h1>
+            <div className="to-do-list">
+            this.state ..
+            </div>
+         </div>
+      )
+   }
+}
 
 export default todoList;
